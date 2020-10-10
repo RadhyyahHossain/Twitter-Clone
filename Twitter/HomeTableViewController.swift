06bf91ce4a2b3e2c45e39baf0bfb794f16449e00
Radhyyah Hossain <radiabxd@gmail.com>
2020-10-09 22:07:48 -0400

    Created a reload button that allows users to reload for new tweets. Next will make it infinite scrolling.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -14,20 +14,26 @@ class HomeTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numberOfTweet: Int!
     
+    let myRefreshControl = UIRefreshControl()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadTweet()
         
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        
+        tableView.refreshControl = myRefreshControl
+        
+        
     }
     
-    
-    
-    func loadTweet(){
+    @objc func loadTweet(){
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
+        let myParams = ["count": 20]
         
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
             
@@ -37,6 +43,8 @@ class HomeTableViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+            
             
         }, failure: { (Error) in
             print("Could not retrieve tweets")
