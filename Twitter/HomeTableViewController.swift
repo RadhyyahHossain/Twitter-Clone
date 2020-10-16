fa58a560c7da46b0751de332bc53fa51b72aa164
Radhyyah Hossain <radiabxd@gmail.com>
2020-10-16 19:51:04 -0400

    Added a tweet page and function, can write tweets and refresh to see the tweet. Tweet should show up right after pressing tweet but it is not working, tweet button is also not aligned, need to fix.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -21,14 +21,18 @@ class HomeTableViewController: UITableViewController {
         super.viewDidLoad()
         
         loadTweets()
-        
-        
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        
         tableView.refreshControl = myRefreshControl
         
-        
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
+    
     
     @objc func loadTweets(){
         
